308ff42b324b88cadf4859052ad5501fe9d96256
Adeik <15350389+vrbouza@users.noreply.github.com>
2021-04-05 20:11:41 +0200

    updates

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -13,41 +13,59 @@ import os
 
 ## Generic
 c.JupyterHub.admin_access = True
-c.Spawner.default_url = '/lab'
+c.Spawner.default_url     = '/lab'
+
+
 
 ## Authenticator
-from jhub_cas_authenticator.cas_auth import CASAuthenticator
-c.JupyterHub.authenticator_class = CASAuthenticator
+#from jhub_cas_authenticator.cas_auth import CASAuthenticator
+#c.JupyterHub.authenticator_class = CASAuthenticator
 
-# The CAS URLs to redirect (un)authenticated users to.
-c.CASAuthenticator.cas_login_url = 'https://cas.uvsq.fr/login'
-c.CASLocalAuthenticator.cas_logout_url = 'https://cas.uvsq/logout'
+## The CAS URLs to redirect (un)authenticated users to.
+#c.CASAuthenticator.cas_login_url = 'https://cas.uvsq.fr/login'
+#c.CASLocalAuthenticator.cas_logout_url = 'https://cas.uvsq/logout'
 
-# The CAS endpoint for validating service tickets.
-c.CASAuthenticator.cas_service_validate_url = 'https://cas.uvsq.fr/serviceValidate'
+## The CAS endpoint for validating service tickets.
+#c.CASAuthenticator.cas_service_validate_url = 'https://cas.uvsq.fr/serviceValidate'
 
-# The service URL the CAS server will redirect the browser back to on successful authentication.
-c.CASAuthenticator.cas_service_url = 'https://%s/hub/login' % os.environ['HOST']
+## The service URL the CAS server will redirect the browser back to on successful authentication.
+#c.CASAuthenticator.cas_service_url = 'https://%s/hub/login' % os.environ['HOST']
 
-c.Authenticator.admin_users = { 'lucadefe' }
+c.Authenticator.admin_users = { 'vrbouza', "root" }
+
+c.JupyterHub.authenticator_class                = 'ldapauthenticator.LDAPAuthenticator'
+c.LDAPAuthenticator.server_hosts                = ['ldaps://gaeipa.geol.uniovi.es:636', 'ldaps://gaeipa.c3.uniovi.es:636']
+c.LDAPAuthenticator.bind_user_dn                = 'uid=admin,cn=users,cn=accounts,dc=hep,dc=uniovi,dc=es'
+c.LDAPAuthenticator.bind_user_password          = ''
+c.LDAPAuthenticator.user_search_base            = 'cn=users,cn=accounts,dc=hep,dc=uniovi,dc=es'
+c.LDAPAuthenticator.user_search_filter          = '(&(objectClass=person)(uid={username}))'
+c.LDAPAuthenticator.user_membership_attribute   = 'memberOf'
+c.LDAPAuthenticator.group_search_base           = 'cn=groups,cn=accounts,dc=hep,dc=uniovi,dc=es'
+c.LDAPAuthenticator.group_search_filter         = '(&(objectClass=ipausergroup)(memberOf={group}))'
+c.LDAPAuthenticator.allowed_groups              = ['cn=jupyterhub-users,cn=groups,cn=accounts,dc=hep,dc=uniovi,dc=es']
+c.LDAPAuthenticator.allow_nested_groups         = True
+c.LDAPAuthenticator.username_pattern            = '[a-zA-Z0-9_.][a-zA-Z0-9_.-]{0,252}[a-zA-Z0-9_.$-]?'
+c.LDAPAuthenticator.create_user_home_dir        = True
+c.LDAPAuthenticator.create_user_home_dir_cmd    = ['mkhomedir_helper']
 
 
 ## Docker spawner
-c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
-c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
-c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+c.JupyterHub.spawner_class      = 'dockerspawner.DockerSpawner'
+c.DockerSpawner.image           = os.environ['DOCKER_JUPYTER_CONTAINER']
+c.DockerSpawner.network_name    = os.environ['DOCKER_NETWORK_NAME']
 # See https://github.com/jupyterhub/dockerspawner/blob/master/examples/oauth/jupyterhub_config.py
-c.JupyterHub.hub_ip = os.environ['HUB_IP']
+c.JupyterHub.hub_ip             = os.environ['HUB_IP']
 
 # user data persistence
 # see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
-notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan'
-c.DockerSpawner.notebook_dir = notebook_dir
-c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
+#notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan'
+#c.DockerSpawner.notebook_dir = notebook_dir
+#c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
+c.DockerSpawner.notebook_dir = "/nfs/fanae/user/{username}"
 
 # Other stuff
 c.Spawner.cpu_limit = 1
-c.Spawner.mem_limit = '10G'
+c.Spawner.mem_limit = '1G'
 
 
 ## Services

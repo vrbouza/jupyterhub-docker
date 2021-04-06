d9dd5c68e4fedd6dda418e66fbe484473d5bf069
Adeik <15350389+vrbouza@users.noreply.github.com>
2021-04-06 19:29:34 +0200

    update

--- pre_start.sh
+++ pre_start.sh
@@ -1,1 +1,2 @@
-cp ./jupyterhub/jupyterhub_config.py /opt/jupyterhub-swaneau/data/srv/jupyterhub/
+cp -f ./jupyterhub/jupyterhub_config.py /opt/jupyterhub-swaneau/data/srv/jupyterhub/
+restorecon -R -v /run/docker.sock
